539dd393a7ebcb322edeb675de8d48753bfcd441
Lucas <correa.l@icloud.com>
2023-02-01 00:01:32 -0300

    Subindo aulas

---
+++ src/database/seeds/20230201024157-criar-usuarios.js
@@ -0,0 +1,33 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        nome: 'Lucas Corrêa',
+        email: 'email@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Joana Dark',
+        email: 'qualquercoisa@email.com.br',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Joao Colorado',
+        email: 'email@colorado.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {},
+  ),
+
+  down: () => {},
+};
