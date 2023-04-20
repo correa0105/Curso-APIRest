64a45b04185898ade760c38e167aa02a7b3cee0e
Lucas Corrêa <65604927+correa0105@users.noreply.github.com>
2023-04-20 20:31:28 -0300

    Delete server.js

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const port = process.env.APP_PORT;
-app.listen(port, () => {
-  console.log();
-  console.log('Server Port', port);
-  console.log(`Running on ${process.env.APP_URL}:${port}`);
-});
